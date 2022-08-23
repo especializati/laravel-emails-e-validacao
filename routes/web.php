ccce6a4eba070e5cffd9a5cd9c7f6462bcd76ae5
Carlos Ferreira <carlos@especializati.com.br>
2022-08-23 15:09:09 -0300

    feat: example render emails

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test-email', function () {
+    // return (new ExampleMail([]))->render();
     Mail::to('test@email.com')
             ->send(new ExampleMail([]));
 
