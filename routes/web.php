d58546b2b1590065048e334a6bf1de1b24129f34
Carlos Ferreira <carlos@especializati.com.br>
2022-08-24 16:40:02 -0300

    feat: auth with Starter Kits

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,24 @@
 <?php
 
-use App\Mail\ExampleMail;
-use App\Mail\UserWelcome;
-use App\Models\User;
-use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test-email-markdown', function () {
-    // return (new UserWelcome())->render();
-
-    // Mail::to('carlos@especializati.com.br')
-    //         ->queue(new UserWelcome);
-
-    // Mail::to('carlos@especializati.com.br')
-    //         ->later(now()->addSeconds(3), new UserWelcome);
-
-    Mail::to('carlos@especializati.com.br')
-            ->send(new UserWelcome);
-
-    return 'ok';
-});
-
-Route::get('/test-email', function () {
-    $user = User::factory()->create();
-    // return (new ExampleMail($user))->render();
-    Mail::to('test@email.com')
-            ->send(new ExampleMail($user));
-
-    return 'ok';
-});
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
